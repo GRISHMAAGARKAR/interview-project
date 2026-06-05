381b2f47616979784d10db81eaaddc76ee5435fe
GRISHMAAGARKAR <grishmaagarkar@gmail.com>
2026-06-05 15:10:42 +0530

    Add Cloudinary for file upload

--- Backend/src/middlewares/file.middleware.js
+++ Backend/src/middlewares/file.middleware.js
@@ -1,6 +1,21 @@
 const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStrorage } = require("multer-storage-cloudinary");
 
-const storage = multer.memoryStorage();
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: "uploads",
+        allowed_formats: ["jpg", "png", "jpeg"]
+    }
+});
+
 const upload = multer({
     storage: storage,
     limits: {
